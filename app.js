71a27adc05e2e44377539a5fba9242f66618ad02
AdarshTechnical <132006822+adarrrsh@users.noreply.github.com>
2024-05-31 02:21:51 +0530

    final for the day

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-app.use(express.static('public')); // Serve static files from the "public" directory
-
-const users = {}; // Object to store users by room
+app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', (socket) => {
     console.log('New user connected');
@@ -16,17 +15,6 @@ io.on('connection', (socket) => {
     socket.on('joinRoom', (roomData) => {
         const [room, username] = roomData;
         socket.join(room);
-        
-        // Add user to the room's user list
-        if (!users[room]) {
-            users[room] = [];
-        }
-        users[room].push(username);
-        
-        // Emit updated user list to the room
-        io.to(room).emit('userList', users[room]);
-        
-        // Emit a message to the room
         io.to(room).emit('message', `${username} has joined the room`);
     });
 
@@ -35,11 +23,6 @@ io.on('connection', (socket) => {
     });
 
     socket.on('disconnect', () => {
-        for (let room in users) {
-            users[room] = users[room].filter(username => username !== socket.username);
-            io.to(room).emit('userList', users[room]);
-            io.to(room).emit('message', `${socket.username} has left the room`);
-        }
         console.log('User disconnected');
     });
 });
